9e640199947758ad13196f8a8df34ee0da4d9da7
Carol <cmc7883@rit.edu>
2013-01-31 19:12:14 -0500

    umm

--- src/DocumentChecker.java
+++ src/DocumentChecker.java
@@ -1,3 +1,6 @@
+import akka.actor.UntypedActor;
+import akka.actor.ActorRef;
+import static akka.actor.Actors.actorOf;
 /**
  * Stub
  * 
@@ -6,21 +9,34 @@
  * @author Carol
  *
  */
-public class DocumentChecker {
+public class DocumentChecker extends UntypedActor {
 
-	public void start(){
+	
+	public void sendPersonToQueue(Person person, ActorRef security) throws InterruptedException{
+		int check = (int) (Math.random() * 100);
+		ActorRef queue;
+		if(check <= 20){
+			rejectPerson(person);
+		}else{		
+			for(int i = 1; i < 4; i ++){
+				queue = actorOf(Queue.class).start();
+				security = actorOf(Queue.class).start());
+				queue.tell(person, security);
+			}
+		}
 		
 	}
 	
-	public void sendPersonToQueue(){
+
+	public void rejectPerson(Person person){
+		System.out.println("Person: " + person.getPersonId() + " has been turned away.");
+	}
+
+
+	@Override
+	public void onReceive(Object arg0) throws Exception {
+		// TODO Auto-generated method stub
 		
 	}
-	
-	public void rejectPerson(){
-		
-	}
-	
-	public void end(){
-		
-	}
+
 }
