d8508fa088d1a231b1398ddca5d221870642f8e2
ajl2612 <ajl2612@gmail.com>
2013-02-03 14:42:40 -0500

    Added container classes to start and stop actors

---
+++ src/Configure.java
@@ -0,0 +1,12 @@
+import akka.actor.ActorRef;
+
+	/**
+	*  Container class for TSA initilization information.
+	*  Serves as flag for actors.  
+	*
+	*  @Author Andrew Lyne III	
+	*/
+
+public class Configure{
+	
+}
---
+++ src/EndDay.java
@@ -0,0 +1,11 @@
+import akka.actor.ActorRef;
+
+	/**
+	*  Container class for TSA shut down information.
+	*  Serves as flag for actors to shut down.  
+	*
+	*  @Author Andrew Lyne III	
+	*/
+public class EndDay {
+
+}
