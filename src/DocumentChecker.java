e1cdb7ed53ae3ce6b6ab72d2ddc36b198243115e
Stephan Wlodarczyk <stephanjwlodarczyk@gmail.com>
2013-02-03 14:17:53 -0500

    Build paths reconfigured.

--- src/DocumentChecker.java
+++ src/DocumentChecker.java
@@ -12,28 +12,10 @@ import static akka.actor.Actors.actorOf;
  */
 public class DocumentChecker extends UntypedActor {
 	
-	public void sendPersonToQueue(Person person, ActorRef security) throws InterruptedException{
-		int check = (int) (Math.random() * 100);
-		ActorRef queue;
-		if(check <= 20){
-			rejectPerson(person);
-		}else{		
-			for(int i = 1; i < 4; i ++){
-				queue = actorOf(Queue.class).start();
-				security = actorOf(Queue.class).start());
-				queue.tell(person, security);
-			}
-		}
-		
-	ActorRef queue;
 	
 	public void onReceive(Object arg0) throws Exception {
 
 		
-	}
-
-	public void rejectPerson(Person person){
-		System.out.println("Person: " + person.getPersonId() + " has been turned away.");
 	}
 
 	public void sendPersonToQueue(Person person, ActorRef security){
